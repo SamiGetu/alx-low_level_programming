4b8df60b7f800fac1a5e5c5eb116a686d684cec2
samuel getachw <falconroman36@gmail.com>
2023-06-19 03:37:19 -0700

    all

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Emtry point
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	putchar('\n');
+	return (0);
+}
