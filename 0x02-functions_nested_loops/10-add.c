6c7f70016237d5b389b85383740da7d1e4bd99b5
samuel getachw <falconroman36@gmail.com>
2023-06-21 08:07:39 -0700

    nested

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,8 @@
+# include "main.h"
+
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
+
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+int _abc(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+	_putchar('\n');
+}
